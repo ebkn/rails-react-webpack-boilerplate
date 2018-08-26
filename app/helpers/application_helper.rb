ce59a608113310acfc79c9dc846ac9cabac4cc65
ebkn <ktennis.mqekr12@gmail.com>
2018-08-26 21:26:31 +0900

    feat: create webpack_path helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def webpack_path(filename)
+    if Rails.env == 'development'
+      ENV['WEBPACK_SERVER_PUBLIC_PATH']
+    else
+      filename
+    end
+  end
 end
