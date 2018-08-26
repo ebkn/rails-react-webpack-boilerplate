1c7677ba1dcda6e9d3817c9e0bcdb91630731934
ebkn <ktennis.mqekr12@gmail.com>
2018-08-26 22:39:25 +0900

    fix: eslint

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello from React
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    Hello from React
+  </div>
+);
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
